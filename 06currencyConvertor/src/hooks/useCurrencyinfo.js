6148b3e5fce509d96c285e91219b31c944998bb8
Nitesh-Kaushik <kaushiknitesh77798@gmail.com>
2024-05-21 15:35:52 +0530

    complete project

--- 06currencyConvertor/src/hooks/useCurrencyinfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyinfo.js
@@ -5,13 +5,17 @@ function useCurrencyInfo(currency){
     const [data, setData] = useState({})
 
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/
-        currency-api@1/latest/currencies/${currency}.json`)
-        .then(res => res.json())
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then(res => {
+            if(!res.ok) {
+                throw new Error(`HTTP ERROR! status: ${res.status}`)
+            }
+            res.json()})
         .then((res) => setData(res[currency]))
+        .catch(error => console.error('Error fetching currency data:', error));
         console.log(data);
 
-    }, [currency])
+    }, [currency]);
     console.log(data);
     return data
 }
