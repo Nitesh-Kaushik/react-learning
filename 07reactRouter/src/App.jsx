7939f514f9500349a50d6e05f88d8ab9e9b89fa9
Nitesh-Kaushik <kaushiknitesh77798@gmail.com>
2024-05-21 17:20:31 +0530

    starting a new project

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <h1 className='bg-green-600 p-4'>Hi there!</h1>
+   </>
+  )
+}
+
+export default App
