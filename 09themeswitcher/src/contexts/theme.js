574e0da53c9e47b605117ea615bb5d6f9a3fa8dc
Nitesh-Kaushik <kaushiknitesh77798@gmail.com>
2024-05-24 18:49:48 +0530

    ThemeSwitcher

--- 09themeswitcher/src/contexts/theme.js
+++ 09themeswitcher/src/contexts/theme.js
@@ -8,8 +8,9 @@ export const ThemeContext = createContext({
 })
 
 
-export const ThemeProvider = ThemeProvider.Provider
+export const ThemeProvider = ThemeContext.Provider
 
 export default function useTheme(){
     return useContext(ThemeContext)
 }
+
