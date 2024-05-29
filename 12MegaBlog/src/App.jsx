ef5c131ac9501e88a4a8a7bcdbba9001c7a2ff0e
Nitesh-Kaushik <kaushiknitesh77798@gmail.com>
2024-05-30 01:07:38 +0530

    ok

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,8 +1,11 @@
 
 import './App.css'
+import conf from './conf/conf';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL );
+  
+  console.log(conf.appwriteCollection);
+  console.log(conf.appwriteUrl);
 
   return (
     <>
--- 12MegaBlog/src/conf/conf.js
+++ 12MegaBlog/src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
-    appwriteCollection: String(import.meta.env.VITE_COLLECTION_ID),
-    appwriteBucket: String(import.meta.env.VITE_BUCKET_ID),
-    appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollection: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucket: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
 }
 
 export default conf
