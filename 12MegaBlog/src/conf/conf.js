f215d5105a5fc9d223a2e4430484e62efbd787f7
Nitesh-Kaushik <kaushiknitesh77798@gmail.com>
2024-05-29 15:24:51 +0530

    START

---
+++ 12MegaBlog/src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteDatabaseId: String(import.meta.env.VITE_PROJECT_ID),
+    appwriteCollection: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteBucket: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_URL),
+}
+
+export default conf
