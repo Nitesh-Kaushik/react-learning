ee88e3126c3f8f894f2008fe023c28e3d45a513c
Nitesh-Kaushik <kaushiknitesh77798@gmail.com>
2024-05-29 15:29:19 +0530

    CONFIG

--- 12MegaBlog/src/conf/conf.js
+++ 12MegaBlog/src/conf/conf.js
@@ -1,9 +1,11 @@
 const conf = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteDatabaseId: String(import.meta.env.VITE_PROJECT_ID),
-    appwriteCollection: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteBucket: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
+    appwriteCollection: String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBucket: String(import.meta.env.VITE_BUCKET_ID),
+    appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
 }
 
 export default conf
+
+
