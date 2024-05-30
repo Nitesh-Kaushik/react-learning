5916f8880edfa3d370c5e39cab5f8e89bd827dfb
Nitesh-Kaushik <kaushiknitesh77798@gmail.com>
2024-05-30 13:53:20 +0530

    LogoutBtn

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,5 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Container from "./container/container.jsx"
 
-
-export {Header, Footer}
+export {Header, Footer, Logo, Container, LogoutBtn}
