458cea1d81d23f625cda49eb2f48d167008a2011
Nitesh-Kaushik <kaushiknitesh77798@gmail.com>
2024-06-02 02:38:17 +0530

    Pages

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Container from "./container/container.jsx"
+import RTE from "./RTE.jsx";
 
-export {Header, Footer, Logo, Container, LogoutBtn}
+
+export {Header, Footer, Logo, Container, LogoutBtn, RTE}
